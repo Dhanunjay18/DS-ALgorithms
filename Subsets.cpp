2bc00debbf0a460f89b0e52c4b982710f279d6e8
Dhanunjay18 <bhavanidhanunjay210@gmail.com>
2022-10-27 19:59:29 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void generateSubsets(vector<int> &arr, vector<vector<int>> &ans, vector<int> &curr, int idx){    
+    ans.push_back(curr);
+    for(int i=idx; i < arr.size();  i++){
+        curr.push_back(arr[i]);        
+        generateSubsets(arr,ans,curr,i+1);        
+        curr.pop_back();
+    }
+    return;
+}
+int main()
+{
+    
+    vector<int> arr = {1,2};
+    
+    vector<vector<int>> ans;
+    vector<int> curr;
+    generateSubsets(arr,ans,curr,0);
+    for(auto i: ans){
+        for(auto j: i) cout<<j<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
